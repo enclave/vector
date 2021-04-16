49ad46841cf5bf3f92ec657fd10689626ee6f671
enclave <82481867+enclave@users.noreply.github.com>
2021-04-16 18:33:58 +0100

    Update vector.v
    Added normalize_fast making use of Quake III Arena's inverse square root function.
    
    Formatted the source code (v fmt).
    
    Return an f64 when using the length method, rather than casting it to the generic type.
    
    Correct the normalize function.
    
    Updated some of the comments.

--- vector.v
+++ vector.v
@@ -9,12 +9,28 @@ mut:
 	z T
 }
 
+// Ported from Quake III Arena
+// https://github.com/id-Software/Quake-III-Arena/blob/master/code/game/q_math.c#L552-#L572
+// Returns the inverse square root of a number
+[unsafe]
+fn inverse_rsqrt(number f32) f32 {
+	threehalfs := f32(1.5)
+	x2 := number * f32(0.5)
+	mut y := number
+	mut i := unsafe { (*(&int(&y))) }
+	i = 0x5f3759df - (i >> 1)
+	y = unsafe { (*(&f32(&i))) }
+	y *= (threehalfs - x2 * y * y)
+	return y
+}
+
 // Adds two vectors and returns a Vector3
 fn (v Vector3<T>) add<T>(v2 Vector3<T>) Vector3<T> {
 	return Vector3{v.x + v2.x, v.y + v2.y, v.z + v2.z}
 }
 
-// Checks each axis of the vector and clamps if an axis goes below the min or beyond the max value
+// Returns a vector with each of its axis checked if it goes below the min or beyond the max value
+// and clamped accordingly
 fn (v Vector3<T>) clamp<T>(min Vector3<T>, max Vector3<T>) Vector3<T> {
 	mut vec := Vector3{}
 
@@ -45,11 +61,7 @@ fn (v Vector3<T>) compare<T>(v2 Vector3<T>) bool {
 
 // Product of two vectors which returns a vector quantity
 fn (v Vector3<T>) cross_product<T>(v2 Vector3<T>) Vector3<T> {
-	return Vector3 {
-		v.y * v2.z - v.z * v2.y,
-		v.z * v2.x - v.x * v2.z,
-		v.x * v2.y - v.y * v2.x
-	}
+	return Vector3{v.y * v2.z - v.z * v2.y, v.z * v2.x - v.x * v2.z, v.x * v2.y - v.y * v2.x}
 }
 
 // Inverses a vector
@@ -58,10 +70,10 @@ fn (v Vector3<T>) inverse<T>() Vector3<T> {
 }
 
 // Returns the vector length
-fn (v Vector3<T>) length<T>() T {
+fn (v Vector3<T>) length<T>() f64 {
 	// length_sqr := v.length_squared<T>()
 	length_sqr := v.x * v.x + v.y * v.y + v.z * v.z
-	return T(math.sqrt(length_sqr))
+	return math.sqrt(length_sqr)
 }
 
 // Returns the vector length squared
@@ -71,11 +83,18 @@ fn (v Vector3<T>) length_squared<T>() T {
 
 // Returns a unit vector
 fn (v Vector3<T>) normalize<T>() Vector3<T> {
-	length_inv := 1 / math.sqrt(length_squared(v))
+	length := math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
+	length_inv := 1 / length
 	return Vector3{v.x * length_inv, v.y * length_inv, v.z * length_inv}
 }
 
-// Multiplies each axis of a vector by a scale factor
+// Returns a unit vector
+fn (v Vector3<T>) normalize_fast<T>() Vector3<T> {
+	length_inv := unsafe { inverse_rsqrt(v.x * v.x + v.y * v.y + v.z * v.z) }
+	return Vector3{v.x * length_inv, v.y * length_inv, v.z * length_inv}
+}
+
+// Returns a vector with each of its axis multiplied by a scale factor
 fn (v Vector3<T>) scale<T>(scale_factor T) Vector3<T> {
 	return Vector3{v.x * scale_factor, v.y * scale_factor, v.z * scale_factor}
 }
